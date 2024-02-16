d3cfeca70d74b69d9cda8429e70d408bc35ffcf0
Vitalie Cherpec <vitalie@penguin.ro>
2024-02-16 18:51:46 +0200

    Add missing delegations

--- templates.lua
+++ templates.lua
@@ -2,7 +2,9 @@ M = {}
 
 local github_pages = require "services.github_pages"
 local google_workspace = require "services.google_workspace"
+local mailbox_org = require "services.mailbox_org"
 local mailgun = require "services.mailgun"
+local migadu = require "services.migadu"
 local netlify = require "services.netlify"
 local office365 = require "services.office365"
 local shopify = require "services.shopify"
@@ -20,9 +22,19 @@ M.google_workspace = function(_, ...)
     return google_workspace.records(table.unpack(arg))
 end
 
+M.mailbox_org = function(_, ...)
+    local arg = { ... }
+    return mailbox_org.records(table.unpack(arg))
+end
+
 M.mailgun = function(_, ...)
     local arg = { ... }
     return mailgun.records(table.unpack(arg))
+end
+
+M.migadu = function(_, ...)
+    local arg = { ... }
+    return migadu.records(table.unpack(arg))
 end
 
 M.netlify = function(_, ...)
